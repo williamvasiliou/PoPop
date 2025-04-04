e07028f583b23780aab70d929e8480794360c20a
williamvasiliou <195391054+williamvasiliou@users.noreply.github.com>
2025-04-04 23:54:33 +0000

    Add files via upload

---
+++ PoPop.pl
@@ -0,0 +1,146 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Math::Trig qw(tanh);
+
+my @C = split(' ', $C);
+my %V = map { $_ => 1 } @C;
+my @V = sort map { $_ } keys %V;
+
+my %Wf = map { $_ => { map { $_ => 0 } @V } } @V;
+my %Wi = map { $_ => { map { $_ => 0 } @V } } @V;
+my %Wo = map { $_ => { map { $_ => 0 } @V } } @V;
+my %Wc = map { $_ => { map { $_ => 0 } @V } } @V;
+
+my %Uf = map { $_ => { map { $_ => 0 } @V } } @V;
+my %Ui = map { $_ => { map { $_ => 0 } @V } } @V;
+my %Uo = map { $_ => { map { $_ => 0 } @V } } @V;
+my %Uc = map { $_ => { map { $_ => 0 } @V } } @V;
+
+my @f = ({ map { $_ => 0 } @V });
+my @i = ({ map { $_ => 0 } @V });
+my @o = ({ map { $_ => 0 } @V });
+my @c = ({ map { $_ => 0 } @V }, { map { $_ => 0 } @V });
+my @h = ({ map { $_ => 0 } @V }, { map { $_ => 0 } @V });
+
+sub sigmoid {
+	return 1 / (1 + 2.718281828459045 ** (-shift));
+}
+
+sub f {
+	my $x = shift;
+	my $h = shift;
+
+	my %w = map { $_ => $Wf{$x}{$_} } @V;
+	foreach my $i (@V) {
+		my $u = $Uf{$i};
+		foreach (@V) {
+			$w{$_} += $u->{$_} * $h->{$i};
+		}
+	}
+
+	foreach (@V) {
+		$w{$_} = sigmoid($w{$_});
+	}
+
+	return \%w;
+}
+
+sub i {
+	my $x = shift;
+	my $h = shift;
+
+	my %w = map { $_ => $Wi{$x}{$_} } @V;
+	foreach my $i (@V) {
+		my $u = $Ui{$i};
+		foreach (@V) {
+			$w{$_} += $u->{$_} * $h->{$i};
+		}
+	}
+
+	foreach (@V) {
+		$w{$_} = sigmoid($w{$_});
+	}
+
+	return \%w;
+}
+
+sub o {
+	my $x = shift;
+	my $h = shift;
+
+	my %w = map { $_ => $Wo{$x}{$_} } @V;
+	foreach my $i (@V) {
+		my $u = $Uo{$i};
+		foreach (@V) {
+			$w{$_} += $u->{$_} * $h->{$i};
+		}
+	}
+
+	foreach (@V) {
+		$w{$_} = sigmoid($w{$_});
+	}
+
+	return \%w;
+}
+
+sub ci {
+	my $x = shift;
+	my $h = shift;
+
+	my %w = map { $_ => $Wc{$x}{$_} } @V;
+	foreach my $i (@V) {
+		my $u = $Uc{$i};
+		foreach (@V) {
+			$w{$_} += $u->{$_} * $h->{$i};
+		}
+	}
+
+	foreach (@V) {
+		$w{$_} = Math::Trig::tanh($w{$_});
+	}
+
+	return \%w;
+}
+
+sub c {
+	my $f = shift;
+	my $c = shift;
+	my $i = shift;
+	my $ci = shift;
+
+	return { map { $_ => $f->{$_} * $c->{$_} + $i->{$_} * $ci->{$_} } @V };
+}
+
+sub h {
+	my $o = shift;
+	my $c = shift;
+
+	return { map { $_ => $o->{$_} * sigmoid($c->{$_}) } @V };
+}
+
+sub y {
+	my $x = shift;
+	$h[0] = $h[1];
+	$f[0] = f($x, $h[0]);
+	$i[0] = i($x, $h[0]);
+	$o[0] = o($x, $h[0]);
+	$c[0] = $c[1];
+	$c[1] = ci($x, $h[0]);
+	$c[1] = c($f[0], $c[0], $i[0], $c[1]);
+	$h[1] = h($o[0], $c[1]);
+
+	my $h = $h[1]->{$V[0]};
+	my $v = 0;
+
+	foreach (my $i = 1; $i < @V; ++$i) {
+		my $y = $h[1]->{$V[$i]};
+		if ($y > $h) {
+			$h = $y;
+			$v = $i;
+		}
+	}
+
+	return $V[$v];
+}
